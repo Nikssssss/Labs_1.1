446d30196215615337702ae0c9bc1b182ac49feb
Nikssssss <43628920+Nikssssss@users.noreply.github.com>
2020-04-28 22:20:30 +0700

    Update Main.java

--- Lab0(Calculator)/Calculator/src/main/java/Main.java
+++ Lab0(Calculator)/Calculator/src/main/java/Main.java
@@ -3,13 +3,14 @@ import calculator.factoryExceptions.PropertyLoadException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Main {
     private static Logger LOGGER = Logger.getLogger(Main.class.getName());
-    public static void main(String[] args) {
+    public static void main(String[] args){
         try {
             LogManager.getLogManager().readConfiguration(new FileInputStream("src/main/resources/loggerConfig.properties"));
         }
@@ -20,19 +21,37 @@ public class Main {
             LOGGER.log(Level.SEVERE, "Too many arguments in main");
             return;
         }
+        InputStream inputFile;
+        try {
+            inputFile = createReader(args);
+        }
+        catch (IOException ex){
+            LOGGER.log(Level.SEVERE, "Can't open input file");
+            return;
+        }
         Calculator calculator = new Calculator();
         LOGGER.log(Level.INFO, "Created Calculator in class {0}", Main.class.getName());
         try {
-            calculator.calculate(args[0]);
+            calculator.calculate(inputFile);
         }
         catch (PropertyLoadException ex){
             LOGGER.log(Level.SEVERE, "Can't find the property file of commands, " + ex.getMessage());
             return;
         }
-        catch (IOException ex){
-            LOGGER.log(Level.SEVERE, "Can't open input file, " + ex.getMessage());
-            return;
-        }
         LOGGER.log(Level.INFO, "Calculator has ended the calculation");
+    }
+
+    private static InputStream createReader(String[] args) throws IOException{
+        InputStream inputFile;
+        String inputFileName;
+        try {
+            inputFileName = args[0];
+        }
+        catch(ArrayIndexOutOfBoundsException ignored){
+            inputFile = System.in;
+            return inputFile;
+        }
+        inputFile = new FileInputStream("src/main/resources/" + inputFileName);
+        return inputFile;
     }
 }

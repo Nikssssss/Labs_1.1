5028637ceccef75ec94950455c3cbcf1af8df766
Nikssssss <43628920+Nikssssss@users.noreply.github.com>
2020-04-28 22:28:01 +0700

    Update Define.java

--- Lab0(Calculator)/Calculator/src/main/java/calculator/commands/Define.java
+++ Lab0(Calculator)/Calculator/src/main/java/calculator/commands/Define.java
@@ -9,7 +9,6 @@ public class Define implements Command {
         data.pushVariable(argsOfCommand[0], Double.parseDouble(argsOfCommand[1]));
     }
 
-    @Override
     public void checkCommand(Context data, String[] argsOfCommand) throws WrongArgumentsException{
         if (argsOfCommand.length != 2){
             throw new WrongArgumentsException("Need only 2 arguments");
